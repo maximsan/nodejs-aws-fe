0b2cff88b289b7e4b9cd45469621cad9fc24a339
maximsan <maximuzz@yandex.ru>
2020-12-06 01:17:12 +0300

    feat: change API urls

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://g7jj8pkjc3.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://g7jj8pkjc3.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://cqdcj2np18.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://dot9p062p8.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://g7jj8pkjc3.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
