88a9e1a36843f9203d3bb4ae473dbd8538eeb83e
maximsan <maksim.s@germanstandard.dev>
2020-12-07 00:29:25 +0300

    feat: add proxy link to cart-api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://g7jj8pkjc3.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://dot9p062p8.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://g7jj8pkjc3.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://maximsan-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://w9wglhsyph.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
